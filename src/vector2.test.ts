5656f0eb0070cee4d16f8e192dcef9148a158848
hopeless programmer <me@hopeless-programmer.online>
2021-04-13 03:39:43 +0300

    Add vec2 accessors

--- src/vector2.test.ts
+++ src/vector2.test.ts
@@ -6,34 +6,95 @@ it('should be a class', () => {
 it('should implement constructor without arguments', () => {
     const a = new Vector2
 
-    expect(a.x).toBe(0)
-    expect(a.y).toBe(0)
+    expect(a).toMatchObject({
+        x : 0, y : 0,
+        r : 0, g : 0,
+        0 : 0, 1 : 0,
+    })
 })
 it('should implement constructor with x argument', () => {
     const a = new Vector2({ x : 5 })
 
-    expect(a.x).toBe(5)
-    expect(a.y).toBe(0)
+    expect(a).toMatchObject({
+        x : 5, y : 0,
+        r : 5, g : 0,
+        0 : 5, 1 : 0,
+    })
 })
 it('should implement constructor with y argument', () => {
     const a = new Vector2({ y : 5 })
 
-    expect(a.x).toBe(0)
-    expect(a.y).toBe(5)
+    expect(a).toMatchObject({
+        x : 0, y : 5,
+        r : 0, g : 5,
+        0 : 0, 1 : 5,
+    })
 })
 it('should implement x getter/setter', () => {
     const a = new Vector2
 
     a.x = 5
 
-    expect(a.x).toBe(5)
+    expect(a).toMatchObject({
+        x : 5, y : 0,
+        r : 5, g : 0,
+        0 : 5, 1 : 0,
+    })
+})
+it('should implement r getter/setter', () => {
+    const a = new Vector2
+
+    a.r = 5
+
+    expect(a).toMatchObject({
+        x : 5, y : 0,
+        r : 5, g : 0,
+        0 : 5, 1 : 0,
+    })
+})
+it('should implement 0 getter/setter', () => {
+    const a = new Vector2
+
+    a[0] = 5
+
+    expect(a).toMatchObject({
+        x : 5, y : 0,
+        r : 5, g : 0,
+        0 : 5, 1 : 0,
+    })
 })
 it('should implement y getter/setter', () => {
     const a = new Vector2
 
     a.y = 5
 
-    expect(a.y).toBe(5)
+    expect(a).toMatchObject({
+        x : 0, y : 5,
+        r : 0, g : 5,
+        0 : 0, 1 : 5,
+    })
+})
+it('should implement g getter/setter', () => {
+    const a = new Vector2
+
+    a.g = 5
+
+    expect(a).toMatchObject({
+        x : 0, y : 5,
+        r : 0, g : 5,
+        0 : 0, 1 : 5,
+    })
+})
+it('should implement 1 getter/setter', () => {
+    const a = new Vector2
+
+    a[1] = 5
+
+    expect(a).toMatchObject({
+        x : 0, y : 5,
+        r : 0, g : 5,
+        0 : 0, 1 : 5,
+    })
 })
 it('should implement toString()', () => {
     const a = new Vector2({ x : 1, y : 2 })
--- src/vector2.ts
+++ src/vector2.ts
@@ -20,6 +20,32 @@ export default class Vector2 {
         this._y = y
     }
 
+    public get r() {
+        return this.x
+    }
+    public set r(r : number) {
+        this.x = r
+    }
+    public get g() {
+        return this.y
+    }
+    public set g(g : number) {
+        this.y = g
+    }
+
+    public get [0]() {
+        return this.x
+    }
+    public set [0](x : number) {
+        this.x = x
+    }
+    public get [1]() {
+        return this.y
+    }
+    public set [1](y : number) {
+        this.y = y
+    }
+
     public toString() {
         const { x, y } = this
 
