4f34f1329f6ee793c4d9c9253f9a24ab5187743a
hopeless programmer <me@hopeless-programmer.online>
2021-04-14 14:28:58 +0300

    Update vec2

--- src/vec2.test.ts
+++ src/vec2.test.ts
@@ -6,6 +6,18 @@ it('should be a function', () => {
 it('should accept single scalar', () => {
     expect(vec2(5)).toMatchObject({ x : 5, y : 5 })
 })
+it('should accept { x | r | 0, y | g | 1 } | number[]', () => {
+    expect(vec2({ x : 1, y : 2 })).toMatchObject({ x : 1, y : 2 })
+    expect(vec2({ x : 1, g : 2 })).toMatchObject({ x : 1, y : 2 })
+    expect(vec2({ x : 1, 1 : 2 })).toMatchObject({ x : 1, y : 2 })
+    expect(vec2({ r : 1, y : 2 })).toMatchObject({ x : 1, y : 2 })
+    expect(vec2({ r : 1, g : 2 })).toMatchObject({ x : 1, y : 2 })
+    expect(vec2({ r : 1, 1 : 2 })).toMatchObject({ x : 1, y : 2 })
+    expect(vec2({ 0 : 1, y : 2 })).toMatchObject({ x : 1, y : 2 })
+    expect(vec2({ 0 : 1, g : 2 })).toMatchObject({ x : 1, y : 2 })
+    expect(vec2({ 0 : 1, 1 : 2 })).toMatchObject({ x : 1, y : 2 })
+    expect(vec2([ 1, 2 ])).toMatchObject({ x : 1, y : 2 })
+})
 it('should accept x and y', () => {
     expect(vec2(1, 2)).toMatchObject({ x : 1, y : 2 })
 })
--- src/vec2.ts
+++ src/vec2.ts
@@ -1,4 +1,5 @@
 import { Vector2 } from './glm'
+import { Vector2Components } from './vector2components'
 
 /**
  * Creates Vector2 with x and y equals to scalar.
@@ -6,14 +7,18 @@ import { Vector2 } from './glm'
  * @param scalar - value to be set as x and y
  */
 export default function vec2(scalar : number) : Vector2;
+export default function vec2(xy : Vector2Components) : Vector2;
 export default function vec2(x : number, y : number) : Vector2;
 
-export default function vec2(x : number, y? : number) {
-    if (y === undefined) return new Vector2({ x, y : x })
-
-    return new Vector2({ x, y })
+export default function vec2(xy : Vector2Components | number, y? : number) {
+    return typeof xy === 'number'
+        ? y === undefined
+            ? Vector2.from(xy)
+            : Vector2.from(xy, y)
+        : Vector2.from(xy)
 }
 
 /**
  * @todo remove semicolons on declarations when syntax highlight will be fixed
+ * @todo check if vec2->Vector2.from passing can be simplified
  */
