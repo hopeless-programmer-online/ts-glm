9fe7f13543349bc2e7a55fdd927c533045e1af35
hopeless programmer <me@hopeless-programmer.online>
2021-04-16 19:31:29 +0300

    Create mat2.ts

---
+++ src/mat2.ts
@@ -0,0 +1,7 @@
+import Matrix2 from './matrix2'
+import Matrix2Values4 from './matrix2-values4'
+
+export default function mat2(values : Matrix2Values4) : Matrix2
+export default function mat2(values : Matrix2Values4) {
+    return new Matrix2({ values })
+}
