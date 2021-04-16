01d98b9c6b50edd3d2037e36d9d1ddddedd04ae9
hopeless programmer <me@hopeless-programmer.online>
2021-04-16 19:33:23 +0300

    Update mat2.ts

--- src/mat2.ts
+++ src/mat2.ts
@@ -1,7 +1,11 @@
 import Matrix2 from './matrix2'
 import Matrix2Values4 from './matrix2-values4'
 
-export default function mat2(values : Matrix2Values4) : Matrix2
+export default function mat2(values : Matrix2Values4) : Matrix2;
 export default function mat2(values : Matrix2Values4) {
     return new Matrix2({ values })
 }
+
+/**
+ * @todo remove semicolons on declarations when syntax highlight will be fixed
+ */
