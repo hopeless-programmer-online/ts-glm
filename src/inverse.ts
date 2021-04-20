691976c29203a081b3399fe649c755132a3ec859
hopeless programmer <me@hopeless-programmer.online>
2021-04-20 05:59:28 +0300

    Add inversion calculation via adj + det

--- src/inverse.ts
+++ src/inverse.ts
@@ -1,20 +1,21 @@
+import adj from './adjugate'
+import det from './determinant'
+import mat2 from './mat2'
 import Matrix2 from './matrix2'
 
 
 export default function inverse(m : Matrix2) : Matrix2;
 
 export default function inverse(m : Matrix2) {
-    const a = m[0][0]
-    const b = m[1][0]
-    const c = m[0][1]
-    const d = m[1][1]
+    const a = adj(m)
+    const d = 1 / det(m)
 
-    return new Matrix2({ values : [
-        1 / (a - b * c / d),
-        1 / (b - a * d / c),
-        1 / (c - a * d / b),
-        1 / (d - b * c / a),
-    ] })
+    return mat2([
+        a[0][0] * d,
+        a[0][1] * d,
+        a[1][0] * d,
+        a[1][1] * d,
+    ])
 }
 
 /**
