149304b43e011145ac6d71fe07998476e211861e
hopeless programmer <me@hopeless-programmer.online>
2021-04-21 05:20:19 +0300

    Update vector2-components.ts

--- src/vector2-components.ts
+++ src/vector2-components.ts
@@ -6,7 +6,7 @@ type Vector2Components =
         & ({ x : number } | { r : number } | { [0] : number })
         & ({ y : number } | { g : number } | { [1] : number })
     )
-    | number[]
+    | [ number, number ]
 
 /**
  * @todo replace with export default when typescript will support it
