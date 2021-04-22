e11cc18d75544109e0096e27e6ea661d90ad940b
hopeless programmer <me@hopeless-programmer.online>
2021-04-22 07:23:21 +0300

    Add Vector3.from

--- src/vector3.ts
+++ src/vector3.ts
@@ -1,6 +1,26 @@
+import deduceVector3 from './deduce-vector3'
 import Vector3Abstraction from './vector3-abstraction'
+import Vector3Components from './vector3-components'
 
 export default class Vector3 extends Vector3Abstraction {
+    public static from(scalar : number) : Vector3
+    public static from(xyz : Vector3Components) : Vector3
+    public static from(x : number, y : number) : Vector3
+    public static from(x : number, y : number, z : number) : Vector3
+    public static from(xyz : Vector3Components | number, y? : number, z? : number) {
+        if (typeof xyz === 'number') {
+            if (y !== undefined) {
+                if (z !== undefined) return new Vector3({ x : xyz, y, z })
+
+                return new Vector3({ x : xyz, y })
+            }
+
+            return new Vector3({ x : xyz, y : xyz, z : xyz })
+        }
+
+        return deduceVector3(xyz)
+    }
+
     private _x : number
     private _y : number
     private _z : number
