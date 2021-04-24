95fe90be671b44240b736d6262819470df1c2a76
hopeless programmer <me@hopeless-programmer.online>
2021-04-24 05:08:07 +0300

    Add dot vec3

--- src/dot.test.ts
+++ src/dot.test.ts
@@ -1,4 +1,4 @@
-import { dot, vec2 } from './glm'
+import { dot, vec2, vec3 } from './glm'
 
 it('should be a function', () => {
     expect(typeof dot).toBe('function')
@@ -17,4 +17,16 @@ it('should accept { r, g }', () => {
 })
 it('should accept [ number, number ]', () => {
     expect(dot([ 1, 2 ], { x : 3, y : 4 })).toBe(1 * 3 + 2 * 4)
+})
+it('should accept vec3', () => {
+    expect(dot(vec3(1, 2, 3), vec3(4, 5, 6))).toBe(1 * 4 + 2 * 5 + 3 * 6)
+})
+it('should accept { x, y, z }', () => {
+    expect(dot({ x : 1, y : 2, z : 3 }, { x : 4, y : 5, z : 6 })).toBe(1 * 4 + 2 * 5 + 3 * 6)
+})
+it('should accept { r, g, b }', () => {
+    expect(dot({ r : 1, g : 2, b : 3 }, { x : 4, y : 5, z : 6 })).toBe(1 * 4 + 2 * 5 + 3 * 6)
+})
+it('should accept [ number, number, number ]', () => {
+    expect(dot([ 1, 2, 3 ], { x : 4, y : 5, z : 6 })).toBe(1 * 4 + 2 * 5 + 3 * 6)
 })
