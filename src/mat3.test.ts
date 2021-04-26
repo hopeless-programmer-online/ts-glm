6d6975054b1dd2718ab7805c2e9f488ccbe729a1
hopeless programmer <me@hopeless-programmer.online>
2021-04-26 06:47:05 +0300

    Add mat3 from numbers

--- src/mat3.test.ts
+++ src/mat3.test.ts
@@ -1,5 +1,16 @@
 import { mat3 } from './glm'
 
+it('should support 9 numbers', () => {
+    const m = mat3(
+        1, 2, 3,
+        4, 5, 6,
+        7, 8, 9,
+    )
+
+    expect(m[0]).toMatchObject({ x : 1, y : 2, z : 3 })
+    expect(m[1]).toMatchObject({ x : 4, y : 5, z : 6 })
+    expect(m[2]).toMatchObject({ x : 7, y : 8, z : 9 })
+})
 it('should support array of 9 numbers', () => {
     const m = mat3([
         1, 2, 3,
--- src/mat3.ts
+++ src/mat3.ts
@@ -1,6 +1,14 @@
 export default function mat3(values : Matrix3Values9) : Matrix3;
-export default function mat3(values : Matrix3Values9) {
-    return new Matrix3({ values })
+export default function mat3(m00 : number, m01 : number, m02 : number, m10 : number, m11 : number, m12 : number, m20 : number, m21 : number, m22 : number) : Matrix3;
+
+export default function mat3(m00 : Matrix3Values9 | number, m01? : number, m02? : number, m10? : number, m11? : number, m12? : number, m20? : number, m21? : number, m22? : number) {
+    if (typeof m00 === 'number') return new Matrix3({ values : [
+        m00, m01, m02,
+        m10, m11, m12,
+        m20, m21, m22,
+    ] })
+
+    return new Matrix3({ values : m00 })
 }
 
 import Matrix3 from './matrix3'
