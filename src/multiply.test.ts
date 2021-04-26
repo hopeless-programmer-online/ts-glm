dd7a69854824a7fbe20d37aa5c75d4f15aa6b8c9
hopeless programmer <me@hopeless-programmer.online>
2021-04-26 07:35:16 +0300

    Add mul overloads

--- src/multiply.test.ts
+++ src/multiply.test.ts
@@ -1,4 +1,4 @@
-import { mul, mat2, vec2 } from './glm'
+import { mul, mat2, vec2, mat3, vec3 } from './glm'
 
 it('should multiply number and number', () => {
     expect(mul(2, 3)).toBe(6)
@@ -16,6 +16,20 @@ it('should multiply number and mat2', () => {
     expect(r[1][0]).toBe(2 * 5)
     expect(r[0][1]).toBe(2 * 4)
     expect(r[1][1]).toBe(2 * 6)
+})
+it('should multiply number and mat3', () => {
+    expect(mul(
+        2,
+        mat3(
+            1, 2, 3,
+            4, 5, 6,
+            7, 8, 9,
+        ),
+    )).toMatchObject({
+        0 : { x : 2 * 1, y : 2 * 2, z : 2 * 3, },
+        1 : { x : 2 * 4, y : 2 * 5, z : 2 * 6, },
+        2 : { x : 2 * 7, y : 2 * 8, z : 2 * 9, },
+    })
 })
 it('should multiply vec2 and number', () => {
     const { x, y } = mul( vec2(1, 2), 3 )
@@ -50,4 +64,32 @@ it('should multiply mat2 and mat2', () => {
     expect(r[1][0]).toBe(1 * 6 + 2 * 8)
     expect(r[0][1]).toBe(3 * 5 + 4 * 7)
     expect(r[1][1]).toBe(3 * 6 + 4 * 8)
+})
+it('should multiply mat3 and number', () => {
+    expect(mul(
+        mat3(
+            1, 2, 3,
+            4, 5, 6,
+            7, 8, 9,
+        ),
+        2,
+    )).toMatchObject({
+        0 : { x : 2 * 1, y : 2 * 2, z : 2 * 3, },
+        1 : { x : 2 * 4, y : 2 * 5, z : 2 * 6, },
+        2 : { x : 2 * 7, y : 2 * 8, z : 2 * 9, },
+    })
+})
+it('should multiply mat3 and vec3', () => {
+    expect(mul(
+        mat3(
+            1, 2, 3,
+            4, 5, 6,
+            7, 8, 9,
+        ),
+        vec3(1, 2, 3),
+    )).toMatchObject({
+        x : 1 * 1 + 2 * 4 + 3 * 7,
+        y : 1 * 2 + 2 * 5 + 3 * 8,
+        z : 1 * 3 + 2 * 6 + 3 * 9,
+    })
 })
--- src/multiply.ts
+++ src/multiply.ts
@@ -1,28 +1,40 @@
+type Left = Matrix3 | Matrix2 | Vector2 | number
+type Right = Matrix3 | Matrix2 | Vector3 | Vector2 | number
+
 export default function multiply(a : number, b : number) : number;
 export default function multiply(a : number, b : Vector2) : Vector2;
 export default function multiply(a : number, b : Matrix2) : Matrix2;
+export default function multiply(a : number, b : Matrix3) : Matrix3;
 export default function multiply(a : Vector2, b : number) : Vector2;
 export default function multiply(a : Vector2, b : Vector2) : Vector2;
 export default function multiply(a : Matrix2, b : number) : Matrix2;
 export default function multiply(a : Matrix2, b : Vector2) : Vector2;
 export default function multiply(a : Matrix2, b : Matrix2) : Matrix2;
+export default function multiply(a : Matrix3, b : number) : Vector3;
+export default function multiply(a : Matrix3, b : Vector3) : Vector3;
 
-export default function multiply(a : Matrix2 | Vector2 | number, b : Matrix2 | Vector2 | number) {
+export default function multiply(a : Left, b : Right) {
     if (typeof a === 'number') {
         if (typeof b === 'number') return a * b
         if (b instanceof Vector2) return multiplyNumber2Vector2(a, b)
+        if (b instanceof Matrix3) return multiplyNumberMatrix3(a, b)
 
-        return multiplyNumberMatrix2(a, b)
+        return multiplyNumberMatrix2(a, b as Matrix2)
     }
     if (a instanceof Vector2) {
         if (typeof b === 'number') return multiplyVector2Number(a, b)
 
         return multiplyVector2Vector2(a, b as Vector2)
     }
-    if (typeof b === 'number') return multiplyMatrix2Number(a, b)
-    if (b instanceof Vector2) return multiplyMatrix2Vector2(a, b)
+    if (a instanceof Matrix2) {
+        if (typeof b === 'number') return multiplyMatrix2Number(a, b)
+        if (b instanceof Vector2) return multiplyMatrix2Vector2(a, b)
 
-    return multiplyMatrix2Matrix2(a, b)
+        return multiplyMatrix2Matrix2(a, b as Matrix2)
+    }
+    if (typeof b === 'number') return multiplyMatrix3Number(a, b)
+
+    return multiplyMatrix3Vector3(a, b as Vector3)
 }
 
 import Vector2 from './vector2'
@@ -34,6 +46,11 @@ import multiplyNumber2Vector2 from './multiply-number-vector2'
 import multiplyVector2Number from './multiply-vector2-number'
 import multiplyNumberMatrix2 from './multiply-number-matrix2'
 import multiplyMatrix2Number from './multiply-matrix2-number'
+import multiplyMatrix3Vector3 from './multiply-matrix3-vector3'
+import Vector3 from './vector3'
+import Matrix3 from './matrix3'
+import multiplyNumberMatrix3 from './multiply-number-matrix3'
+import multiplyMatrix3Number from './multiply-matrix3-number'
 
 /**
  * @todo remove semicolons on declarations when syntax highlight will be fixed
