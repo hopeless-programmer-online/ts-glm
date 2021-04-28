c23ad6efd8ba51d2bb207411302f90a7c38ae820
hopeless programmer <me@hopeless-programmer.online>
2021-04-28 21:57:48 +0300

    Add empty vec3 overload

--- src/vec3.test.ts
+++ src/vec3.test.ts
@@ -1,5 +1,12 @@
 import { vec3, Vector3 } from './glm'
 
+it('should accept nothing', () => {
+    expect(vec3()).toMatchObject<Vector3>({
+        x : Vector3.default.x,
+        y : Vector3.default.y,
+        z : Vector3.default.z,
+    })
+})
 it('should accept x, y, z', () => {
     expect(vec3(1, 2, 3)).toMatchObject<Vector3>({
         x : 1,
--- src/vec3.ts
+++ src/vec3.ts
@@ -1,4 +1,9 @@
-export default function vec3(x : number, y : number, z : number) : Vector3 {
+export default function vec3() : Vector3;
+export default function vec3(x : number, y : number, z : number) : Vector3;
+
+export default function vec3(x? : number, y? : number, z? : number) : Vector3 {
+    if (x === undefined) return new NumberVector3
+
     return new NumberVector3({ x, y, z })
 }
 
