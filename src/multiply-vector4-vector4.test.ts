3c4dd4541f813a3f1f9f9af68181edb74ba91d3f
hopeless programmer <me@hopeless-programmer.online>
2021-04-28 21:51:46 +0300

    Replace NumberVector4 with vec4

--- src/multiply-vector4-vector4.test.ts
+++ src/multiply-vector4-vector4.test.ts
@@ -1,10 +1,7 @@
-import { multiplyVector4Vector4, NumberVector4 } from './glm'
+import { multiplyVector4Vector4, vec4 } from './glm'
 
 it('should multiply NumberVector4 and NumberVector4', () => {
-    expect(multiplyVector4Vector4(
-        new NumberVector4({ x : 1, y : 2, z : 3, w : 4 }),
-        new NumberVector4({ x : 5, y : 6, z : 7, w : 8 }),
-    )).toMatchObject({
+    expect(multiplyVector4Vector4( vec4(1, 2, 3, 4), vec4(5, 6, 7, 8) )).toMatchObject({
         x : 1 * 5,
         y : 2 * 6,
         z : 3 * 7,
--- src/subtract-vector4-vector4.test.ts
+++ src/subtract-vector4-vector4.test.ts
@@ -1,10 +1,7 @@
-import { subtractVector4Vector4, NumberVector4 } from './glm'
+import { subtractVector4Vector4, vec4 } from './glm'
 
 it('should subtract NumberVector4 and NumberVector4', () => {
-    expect(subtractVector4Vector4(
-        new NumberVector4({ x : 1, y : 2, z : 3, w : 4 }),
-        new NumberVector4({ x : 5, y : 6, z : 7, w : 8 }),
-    )).toMatchObject({
+    expect(subtractVector4Vector4( vec4(1, 2, 3, 4), vec4(5, 6, 7, 8) )).toMatchObject({
         x : 1 - 5,
         y : 2 - 6,
         z : 3 - 7,
