4c99c86a31a02e3b8e59c9914e7deed013080f1a
hopeless programmer <me@hopeless-programmer.online>
2021-05-08 22:38:26 +0300

    Add mat2 from mat2/3/4

--- src/mat2.test.ts
+++ src/mat2.test.ts
@@ -1,4 +1,4 @@
-import { mat2, transposeMatrix2x2 } from './glm'
+import { mat2, mat3, mat4, transposeMatrix2x2 } from './glm'
 
 it('should accept nothing', () => {
     expect(mat2()).toMatchObject({
@@ -13,6 +13,36 @@ it('should accept 4 numbers', () => {
     )).toMatchObject({
         0 : { x : 1, y : 2 },
         1 : { x : 3, y : 4 },
+    })
+})
+it('should accept mat2', () => {
+    expect(mat2(mat2(
+        1, 2,
+        3, 4,
+    ))).toMatchObject({
+        0 : { x : 1, y : 2 },
+        1 : { x : 3, y : 4 },
+    })
+})
+it('should accept mat3', () => {
+    expect(mat2(mat3(
+        1, 2, 3,
+        4, 5, 6,
+        7, 8, 9,
+    ))).toMatchObject({
+        0 : { x : 1, y : 2 },
+        1 : { x : 4, y : 5 },
+    })
+})
+it('should accept mat4', () => {
+    expect(mat2(mat4(
+        1,  2,  3,  4,
+        5,  6,  7,  8,
+        9,  10, 11, 12,
+        13, 14, 15, 16,
+    ))).toMatchObject({
+        0 : { x : 1,  y : 2,  },
+        1 : { x : 5,  y : 6,  },
     })
 })
 it('should export transpose', () => {
--- src/mat2.ts
+++ src/mat2.ts
@@ -1,17 +1,36 @@
+export default function mat2(
+    m00 : number, m01 : number,
+    m10 : number, m11 : number,
+) : Matrix2x2;
+export default function mat2(m : Matrix4x4) : Matrix2x2;
+export default function mat2(m : Matrix3x3) : Matrix2x2;
+export default function mat2(m : Matrix2x2) : Matrix2x2;
 export default function mat2() : Matrix2x2;
-export default function mat2(m00 : number, m01 : number, m10 : number, m11 : number) : Matrix2x2;
 
-export default function mat2(m00? : number, m01? : number, m10? : number, m11? : number) {
-    if (m00 === undefined) return new ArrayMatrix2x2
-
-    return new ArrayMatrix2x2({ values : [
-        m00, m01,
+export default function mat2(
+    m?   : Matrix4x4 | Matrix3x3 | Matrix2x2 | number, m01? : number,
+    m10? : number,                                     m11? : number,
+) {
+    if (m === undefined) return new ArrayMatrix2x2
+    if (typeof m === 'number') return new ArrayMatrix2x2({ values : [
+        m,   m01,
         m10, m11,
     ] })
+    if ('3' in m) return matrix2x2FromMatrix4x4(m)
+    if ('2' in m) return matrix2x2FromMatrix3x3(m)
+
+    return mat2(
+        m[0][0], m[0][1],
+        m[1][0], m[1][1],
+    )
 }
 
-import ArrayMatrix2x2 from './array-matrix2x2'
 import Matrix2x2 from './matrix2x2'
+import Matrix3x3 from './matrix3x3'
+import Matrix4x4 from './matrix4x4'
+import ArrayMatrix2x2 from './array-matrix2x2'
+import matrix2x2FromMatrix3x3 from './matrix2x2-from-matrix3x3'
+import matrix2x2FromMatrix4x4 from './matrix2x2-from-matrix4x4'
 import transposeMatrix2x2 from './transpose-matrix2x2'
 
 mat2.transpose = transposeMatrix2x2
