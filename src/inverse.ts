71ef1e4df89c2cb7a47a678c660419e71b03247f
hopeless programmer <me@hopeless-programmer.online>
2021-06-19 01:06:29 +0300

    Add duck typing for inv

--- src/inverse.ts
+++ src/inverse.ts
@@ -1,12 +1,17 @@
-export default function inverse(m : Matrix2x2) : Matrix2x2;
-export default function inverse(m : Matrix3x3) : Matrix3x3;
-export default function inverse(m : Matrix4x4) : Matrix4x4;
+export default function inverse(m : Matrix2x2Like) : Matrix2x2;
+export default function inverse(m : Matrix3x3Like) : Matrix3x3;
+export default function inverse(m : Matrix4x4Like) : Matrix4x4;
 
-export default function inverse(m : Matrix4x4 | Matrix3x3 | Matrix2x2) {
-    if ('3' in m) return inverseMatrix4x4(m)
-    if ('2' in m) return inverseMatrix3x3(m)
-
-    return inverseMatrix2x2(m)
+export default function inverse(m : Matrix4x4Like | Matrix3x3Like | Matrix2x2Like) {
+    return deduce(m,
+        m => inverseMatrix4x4(m),
+        m => inverseMatrix3x3(m),
+        m => inverseMatrix2x2(m),
+        null,
+        null,
+        null,
+        null,
+    )
 }
 
 import Matrix2x2 from './matrix2x2'
@@ -15,3 +20,8 @@ import Matrix4x4 from './matrix4x4'
 import inverseMatrix2x2 from './inverse-matrix2x2'
 import inverseMatrix3x3 from './inverse-matrix3x3'
 import inverseMatrix4x4 from './inverse-matrix4x4'
+import Matrix2x2Like from './matrix2x2-like'
+import Matrix3x3Like from './matrix3x3-like'
+import Matrix4x4Like from './matrix4x4-like'
+import deduce from './deduce'
+
