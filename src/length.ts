f29f81cb801427c393dee46f9be124ac5314d5d6
hopeless programmer <me@hopeless-programmer.online>
2021-06-19 01:11:47 +0300

    Add duck typing for length

--- src/length.ts
+++ src/length.ts
@@ -1,17 +1,24 @@
-export default function length(v : Vector2) : number;
-export default function length(v : Vector3) : number;
-export default function length(v : Vector4) : number;
+export default function length(v : Vector2Like) : number;
+export default function length(v : Vector3Like) : number;
+export default function length(v : Vector4Like) : number;
 
-export default function length(v : Vector4 | Vector3 | Vector2) {
-    if ('w' in v) return lengthVector4(v)
-    if ('z' in v) return lengthVector3(v)
-
-    return lengthVector2(v)
+export default function length(v : Vector4Like | Vector3Like | Vector2Like) {
+    return deduce(v,
+        null,
+        null,
+        null,
+        v => lengthVector4(v),
+        v => lengthVector3(v),
+        v => lengthVector2(v),
+        null,
+    )
 }
 
-import Vector2 from './vector2'
-import Vector3 from './vector3'
-import Vector4 from './vector4'
 import lengthVector4 from './length-vector4'
 import lengthVector3 from './length-vector3'
 import lengthVector2 from './length-vector2'
+import Vector2Like from './vector2-like'
+import Vector3Like from './vector3-like'
+import Vector4Like from './vector4-like'
+import deduce from './deduce'
+
