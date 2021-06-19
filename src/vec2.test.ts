cdab97e991a65c8f0da9b38d10e3601009a690f7
hopeless programmer <me@hopeless-programmer.online>
2021-06-19 04:56:06 +0300

    Add duck typing for vec2

--- src/vec2.test.ts
+++ src/vec2.test.ts
@@ -21,3 +21,51 @@ it('should accept x and y', () => {
         y : 2,
     })
 })
+it('should accept { x }', () => {
+    expect(vec2({ x : 1 })).toMatchObject({
+        x : 1,
+        y : Vector2.default.y,
+    })
+})
+it('should accept { y }', () => {
+    expect(vec2({ y : 1 })).toMatchObject({
+        x : Vector2.default.x,
+        y : 1,
+    })
+})
+it('should accept { x, y }', () => {
+    expect(vec2({ x : 1, y : 2 })).toMatchObject({
+        x : 1,
+        y : 2,
+    })
+})
+it('should accept {}', () => {
+    expect(vec2({})).toMatchObject({
+        x : Vector2.default.x,
+        y : Vector2.default.y,
+    })
+})
+it('should accept { 0 }', () => {
+    expect(vec2({ 0 : 1 })).toMatchObject({
+        x : 1,
+        y : Vector2.default.y,
+    })
+})
+it('should accept { 1 }', () => {
+    expect(vec2({ 1 : 1 })).toMatchObject({
+        x : Vector2.default.x,
+        y : 1,
+    })
+})
+it('should accept [ number, number ]', () => {
+    expect(vec2([ 1, 2 ])).toMatchObject({
+        x : 1,
+        y : 2,
+    })
+})
+it('should accept []', () => {
+    expect(vec2([])).toMatchObject({
+        x : Vector2.default.x,
+        y : Vector2.default.y,
+    })
+})
