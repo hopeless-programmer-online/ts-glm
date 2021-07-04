7706a5c8dd67dbf2e9402a06ff5a17ccc738cf9e
hopeless programmer <me@hopeless-programmer.online>
2021-07-04 04:24:16 +0300

    Remove files

--- src/vector2-like.ts
+++
@@ -1,8 +0,0 @@
-type Vector2Like =
-    | (
-        & ({ x : number } | { 0 : number })
-        & ({ y : number } | { 1 : number })
-    )
-    | [ number, number ]
-
-export default Vector2Like
--- src/vector3-like.ts
+++
@@ -1,9 +0,0 @@
-type Vector3Like =
-    | (
-        & ({ x : number } | { 0 : number })
-        & ({ y : number } | { 1 : number })
-        & ({ z : number } | { 2 : number })
-    )
-    | [ number, number, number ]
-
-export default Vector3Like
