3acaead3ad705409d2d1f758dba908750f193edc
Hopeless Programmer <me@hopeless-programmer.online>
2021-11-20 18:31:46 +0200

    Fix types

--- src/negate.ts
+++ src/negate.ts
@@ -1,6 +1,6 @@
-export default function negate(v : Vector4Like) : number;
-export default function negate(v : Vector3Like) : number;
-export default function negate(v : Vector2Like) : number;
+export default function negate(v : Vector4Like) : Vector4;
+export default function negate(v : Vector3Like) : Vector3;
+export default function negate(v : Vector2Like) : Vector2;
 export default function negate(v : number)  : number;
 
 export default function negate(v : Vector4Like | Vector3Like | Vector2Like | number) {
@@ -15,6 +15,9 @@ export default function negate(v : Vector4Like | Vector3Like | Vector2Like | num
     )
 }
 
+import Vector2 from './vector2'
+import Vector3 from './vector3'
+import Vector4 from './vector4'
 import Vector4Like from './vector4-like'
 import Vector3Like from './vector3-like'
 import Vector2Like from './vector2-like'
@@ -22,4 +25,3 @@ import negateVector2 from './negate-vector2'
 import negateVector3 from './negate-vector3'
 import negateVector4 from './negate-vector4'
 import deduce from './deduce'
-
--- src/square.ts
+++ src/square.ts
@@ -1,6 +1,6 @@
-export default function square(v : Vector4Like) : number;
-export default function square(v : Vector3Like) : number;
-export default function square(v : Vector2Like) : number;
+export default function square(v : Vector4Like) : Vector4;
+export default function square(v : Vector3Like) : Vector3;
+export default function square(v : Vector2Like) : Vector2;
 export default function square(v : number)  : number;
 
 export default function square(v : Vector4Like | Vector3Like | Vector2Like | number) {
@@ -15,6 +15,9 @@ export default function square(v : Vector4Like | Vector3Like | Vector2Like | num
     )
 }
 
+import Vector2 from './vector2'
+import Vector3 from './vector3'
+import Vector4 from './vector4'
 import Vector4Like from './vector4-like'
 import Vector3Like from './vector3-like'
 import Vector2Like from './vector2-like'
@@ -22,4 +25,3 @@ import squareVector2 from './square-vector2'
 import squareVector3 from './square-vector3'
 import squareVector4 from './square-vector4'
 import deduce from './deduce'
-
